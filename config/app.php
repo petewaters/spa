342da7476528a8124ab7e57607d8cbd09f5e056b
Pete Waters <pete.d.waters@gmail.com>
2017-08-13 18:06:24 +0000

    Removed default auth controllers

--- config/app.php
+++ config/app.php
@@ -162,11 +162,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
