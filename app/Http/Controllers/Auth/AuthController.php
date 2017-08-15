d7ce72b1327242285d9d3ef84ea017660cc2a89c
Pete Waters <pete.d.waters@gmail.com>
2017-08-15 21:58:39 +0000

    Added login method, route and form request

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
-use Tymon\JWTAuth\JWTAuth;
-use App\Http\Requests\User\RegisterFormRequest;
 
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTExceptions\JWTException;
+
+use App\Http\Requests\User\{LoginFormRequest, RegisterFormRequest};
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -16,6 +18,32 @@ class AuthController extends Controller
     public function __construct(JWTAuth $auth)
     {
         $this->auth = $auth;
+    }
+
+    public function login(LoginFormRequest $request)
+    {
+        try {
+            if (!$token = $this->auth->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'errors' => [
+                        'root' => 'Unable to sign in with those credentials.',
+                    ]
+                ], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'errors' => [
+                    'root' => 'Failed: ' . $e,
+                ]
+            ], $e->getStatusCode());
+        }
+
+        return response()->json([
+            'data' => $request->user(),
+            'meta' => [
+                'token' => $token,
+            ]
+        ]);
     }
 
     public function register(RegisterFormRequest $request)
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 use Illuminate\Http\Request;
 
 Route::post('/register', 'Auth\AuthController@register');
+
+Route::post('/login', 'Auth\AuthController@login');
