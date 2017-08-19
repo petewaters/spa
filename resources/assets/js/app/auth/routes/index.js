447475ab5bbb9db8e2e5dccae501bee1adf8b066
Pete Waters <pete.d.waters@gmail.com>
2017-08-19 13:33:49 +0100

    Created and registered login and register vue components and exposed to app.

---
+++ resources/assets/js/app/auth/routes/index.js
@@ -0,0 +1,14 @@
+import { Login, Register } from '../components'
+
+export default [
+    {
+        path: '/login',
+        component: Login,
+        name: 'login'
+    },
+    {
+        path: '/register',
+        component: Register,
+        name: 'register'
+    }
+]
